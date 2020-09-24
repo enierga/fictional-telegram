dfa7b487d681ea6504707ada8ccdb3d3b1a84f6c
venierga <venierga@csvenierga.attlocal.net>
2020-09-24 18:07:56 -0400

    added graphql schemas

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,24 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if user is logged in
 
-module.exports = mutations;
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+
+    return item;
+  }
+
+  // createDog(parent, args, context, info) {
+  //   global.dogs = global.dogs || [];
+    
+  //   // here we create a dog and push into global dogs arr
+  //   const newDog  = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
+};
+
+module.exports = Mutations;
